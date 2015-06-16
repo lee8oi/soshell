4c00c8bc83314d082de33dec4d2d7a1ca5d7cf15
Lee Forest <lee8oi@gmail.com>
2015-06-16 15:14:56 -0400

    Added some simple doc comments to the functions.

--- users.go
+++ users.go
@@ -16,21 +16,25 @@ type user struct {
 	Email, Name string
 }
 
+// isEmail makes she that email is properly formated as an email address.
 func isEmail(email string) bool {
 	reg := regexp.MustCompile("^([\\w\\.\\-_]+)?\\w+@[\\w-_]+(\\.\\w+){1,}$")
 	return reg.MatchString(email)
 }
 
+// isName checks if name only contains word characters.
 func isName(name string) bool {
 	nameReg := regexp.MustCompile("[\\W]+")
 	return !nameReg.MatchString(name)
 }
 
+// load is used to load a users info from json stored in an encrypted file.
 func (u *user) load(name, pass string) error {
 	path := *users + SEP + indexPath([]byte(name))
 	return loadObject(u, path+SEP+"user", pass)
 }
 
+// save will save a users info as json in an encrypted file.
 func (u *user) save(name, pass string) error {
 	path := *users + SEP + indexPath([]byte(name))
 	if !pathExists(path) {
